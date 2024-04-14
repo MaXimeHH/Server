aeedaf915b9b8b845b2282edf6ec11e3e51671ba
MaXimeHH <maxime.debruyne1@epsi.fr>
2024-04-14 14:43:17 +0200

    feat/securisation: ajout du JWTService

--- src/main/java/com/back/customers/controller/CustomerController.java
+++ src/main/java/com/back/customers/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -103,5 +104,10 @@ public class CustomerController implements CustomersApiInterface {
         } catch (Exception exception) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("test")
+    public String getTest() {
+        return "a value";
     }
 }
---
+++ src/main/java/com/back/customers/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.back.customers.controller;
+
+import com.back.customers.services.JWTService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    private final JWTService jwtService;
+
+    public LoginController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public String getToken(Authentication authentication) {
+        return jwtService.generateToken(authentication);
+    }
+}
